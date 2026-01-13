51273c3bd71be749fefbec8e6cfbbe693cd57e4a
William Droz <william.droz@idiap.ch>
2026-01-13 17:52:21 +0100

    chore: basic nicegui app

---
+++ grimoireplot/server.py
@@ -0,0 +1,19 @@
+import random
+from nicegui import app, ui
+
+
+@app.get("/random/{max}")
+def generate_random_number(max: int):
+    return {"min": 0, "max": max, "value": random.randint(0, max)}
+
+
+@ui.page("/")
+def page():
+    max = ui.number("max", value=100)
+    ui.button(
+        "generate random number",
+        on_click=lambda: ui.navigate.to(f"/random/{max.value:.0f}"),
+    )
+
+
+ui.run()
