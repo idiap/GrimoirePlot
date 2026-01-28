e2f2d4cd1ddcafc5d0513d9565b44e27d2c427e5
William Droz <william.droz@idiap.ch>
2026-01-28 17:27:26 +0100

    chore: create db when starting the app

--- grimoireplot/server.py
+++ grimoireplot/server.py
@@ -1,8 +1,11 @@
 import random
 from nicegui import app, ui
+from grimoireplot.models import create_db_and_tables
 
 
 def my_app():
+    create_db_and_tables()
+
     @app.get("/random/{max}")
     def generate_random_number(max: int):
         return {"min": 0, "max": max, "value": random.randint(0, max)}
