6d9fce91b62f012a67fd39a71292a197393f1f7a
William Droz <william.droz@idiap.ch>
2026-01-31 21:10:13 +0100

    feat: endpoint to receive new plots

--- grimoireplot/server.py
+++ grimoireplot/server.py
@@ -1,21 +1,40 @@
-import random
+from dotenv import load_dotenv
+from fastapi import HTTPException, Request
 from nicegui import app, ui
-from grimoireplot.models import create_db_and_tables
+from grimoireplot.models import AddPlotRequest, create_db_and_tables, add_plot
+import os
+from loguru import logger
+
+load_dotenv()
+
+if (_GRIMOIRE_SECRET := os.environ.get("GRIMOIRE_SECRET")) is None:
+    logger.warning("GRIMOIRE_SECRET not set; using default secret")
+    _GRIMOIRE_SECRET = "IDidntSetASecret"
+
+
+def verify_secret(request: Request):
+    if (secret := request.headers.get("grimoire-secret")) is None:
+        raise HTTPException(status_code=401, detail="grimoire-secret not found")
+    if secret != _GRIMOIRE_SECRET:
+        raise HTTPException(status_code=403, detail="invalid grimoire-secret")
 
 
 def my_app():
     create_db_and_tables()
 
-    @app.get("/random/{max}")
-    def generate_random_number(max: int):
-        return {"min": 0, "max": max, "value": random.randint(0, max)}
+    @app.post("/add_plot")
+    def add_plot_endpoint(add_plot_request: AddPlotRequest, request: Request):
+        verify_secret(request)
+        plot = add_plot(
+            grimoire_name=add_plot_request.grimoire_name,
+            chapter_name=add_plot_request.chapter_name,
+            plot_name=add_plot_request.plot_name,
+            json_data=add_plot_request.json_data,
+        )
+        return {"status": "success", "plot_name": plot.name}
 
     @ui.page("/")
     def page():
-        max = ui.number("max", value=100)
-        ui.button(
-            "generate random number",
-            on_click=lambda: ui.navigate.to(f"/random/{max.value:.0f}"),
-        )
+        pass
 
     ui.run()
