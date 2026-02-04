aa059b173201e2307713e0393e07ecc775ea24a6
William Droz <william.droz@idiap.ch>
2026-02-04 14:41:08 +0100

    chore: module that contains commons functions/tools for client and server

---
+++ grimoireplot/common.py
@@ -0,0 +1,28 @@
+"""
+Common module for grimoireplot client and server
+"""
+
+import os
+from dotenv import load_dotenv
+from loguru import logger
+
+
+load_dotenv()
+
+
+def get_grimoire_secret() -> str:
+    if (grimoire_secret := os.environ.get("GRIMOIRE_SECRET")) is None:
+        logger.warning("GRIMOIRE_SECRET not set; using default secret")
+        grimoire_secret = "IDidntSetASecret"
+
+    return grimoire_secret
+
+
+def get_grimoire_server() -> str:
+    if (grimoire_server := os.environ.get("GRIMOIRE_SERVER")) is None:
+        grimoire_server = "http://localhost:8080"
+        logger.warning(
+            f"GRIMOIRE_SERVER not set; using default server {grimoire_server}"
+        )
+
+    return grimoire_server
